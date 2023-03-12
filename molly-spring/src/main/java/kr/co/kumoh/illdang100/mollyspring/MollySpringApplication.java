d55e958da355ea288ab3145b790d72389242429b
JYeonJun <97449471+JYeonJun@users.noreply.github.com>
2023-03-12 13:07:20 +0900

    create(server): BaseTimeEntity

--- molly-spring/src/main/java/kr/co/kumoh/illdang100/mollyspring/MollySpringApplication.java
+++ molly-spring/src/main/java/kr/co/kumoh/illdang100/mollyspring/MollySpringApplication.java
@@ -2,8 +2,10 @@ package kr.co.kumoh.illdang100.mollyspring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class MollySpringApplication {
 
 	public static void main(String[] args) {
