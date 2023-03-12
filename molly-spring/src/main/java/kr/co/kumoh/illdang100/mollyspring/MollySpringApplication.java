979c5f120c9ca87df0aa7861723257b0e6b9555d
JYeonJun <97449471+JYeonJun@users.noreply.github.com>
2023-03-12 12:49:34 +0900

    init(server): spring 서버 초기화

---
+++ molly-spring/src/main/java/kr/co/kumoh/illdang100/mollyspring/MollySpringApplication.java
@@ -0,0 +1,13 @@
+package kr.co.kumoh.illdang100.mollyspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MollySpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MollySpringApplication.class, args);
+	}
+
+}
---
+++ molly-spring/src/test/java/kr/co/kumoh/illdang100/mollyspring/MollySpringApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.kumoh.illdang100.mollyspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MollySpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
