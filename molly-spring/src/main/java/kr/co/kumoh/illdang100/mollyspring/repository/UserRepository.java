6d9f407b306dc0c1409f001962fd2fc7427b95bb
JYeonJun <97449471+JYeonJun@users.noreply.github.com>
2023-03-12 21:39:22 +0900

    feat(server): UserRepository - findByUsername() 생성

--- molly-spring/src/main/java/kr/co/kumoh/illdang100/mollyspring/repository/UserRepository.java
+++ molly-spring/src/main/java/kr/co/kumoh/illdang100/mollyspring/repository/UserRepository.java
@@ -4,4 +4,6 @@ import kr.co.kumoh.illdang100.mollyspring.domain.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    User findByUsername(String username);
 }
