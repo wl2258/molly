299edac94fd44d2f43c30c1bedb43b0dc69d32cb
JYeonJun <97449471+JYeonJun@users.noreply.github.com>
2023-03-12 22:47:24 +0900

    update(server): UserRepository
    findByUsername 반환값을 User에서 Optional<User>로 변경

--- molly-spring/src/main/java/kr/co/kumoh/illdang100/mollyspring/repository/UserRepository.java
+++ molly-spring/src/main/java/kr/co/kumoh/illdang100/mollyspring/repository/UserRepository.java
@@ -3,7 +3,9 @@ package kr.co.kumoh.illdang100.mollyspring.repository;
 import kr.co.kumoh.illdang100.mollyspring.domain.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    User findByUsername(String username);
+    Optional<User> findByUsername(String username);
 }
