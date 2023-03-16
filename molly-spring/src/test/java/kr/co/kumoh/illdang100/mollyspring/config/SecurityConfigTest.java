e78778178a64dc9a9d2ad46206c9e5cdbdb12fa4
JYeonJun <97449471+JYeonJun@users.noreply.github.com>
2023-03-16 19:26:55 +0900

    test(server): SecurityConfig

---
+++ molly-spring/src/test/java/kr/co/kumoh/illdang100/mollyspring/config/SecurityConfigTest.java
@@ -0,0 +1,56 @@
+package kr.co.kumoh.illdang100.mollyspring.config;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+import static org.assertj.core.api.Assertions.*;
+
+@AutoConfigureMockMvc // Mock (가짜) 환경에 MockMvc가 등록된다.
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+public class SecurityConfigTest {
+
+    // 가짜 환경에 등록된 MockMvc를 DI한다.
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void authentication_test() throws Exception {
+
+        // given
+
+        // when
+        ResultActions resultActions = mvc.perform(get("/api/s/hello"));
+        String responseBody = resultActions.andReturn().getResponse().getContentAsString();
+        int httpStatusCode = resultActions.andReturn().getResponse().getStatus();
+        System.out.println("테스트 : " + responseBody);
+        System.out.println("테스트 : " + httpStatusCode);
+
+        // then
+        assertThat(httpStatusCode).isEqualTo(401);
+    }
+
+    @Test
+    public void authorization_test() throws Exception {
+
+        // given
+
+        // when
+        ResultActions resultActions = mvc.perform(get("/api/admin/hello"));
+        String responseBody = resultActions.andReturn().getResponse().getContentAsString();
+        int httpStatusCode = resultActions.andReturn().getResponse().getStatus();
+        System.out.println("테스트 : " + responseBody);
+        System.out.println("테스트 : " + httpStatusCode);
+
+        // then
+        // 로그인이 되어 있지 않은 상태이므로 403이 아닌 401 에러가 발생하는 것이 맞다.
+        assertThat(httpStatusCode).isEqualTo(401);
+
+    }
+}
