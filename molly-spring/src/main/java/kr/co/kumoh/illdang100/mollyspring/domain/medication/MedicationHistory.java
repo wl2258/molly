5fcd0ab833dda6f2bdcb7102da7c2e339308abd6
wl2258 <77067383+wl2258@users.noreply.github.com>
2023-03-17 10:57:26 +0900

    create(server): MedicationHistory 엔티티 필드 추가

--- molly-spring/src/main/java/kr/co/kumoh/illdang100/mollyspring/domain/medication/MedicationHistory.java
+++ molly-spring/src/main/java/kr/co/kumoh/illdang100/mollyspring/domain/medication/MedicationHistory.java
@@ -1,18 +1,29 @@
 package kr.co.kumoh.illdang100.mollyspring.domain.medication;
 
+import kr.co.kumoh.illdang100.mollyspring.domain.BaseTimeEntity;
+import kr.co.kumoh.illdang100.mollyspring.domain.pet.Pet;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import java.time.LocalDate;
 
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class MedicationHistory {
+public class MedicationHistory extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "medical_history_id")
     private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "pet_id")
+    private Pet pet;
+
+    private LocalDate medicationDate;
+
+    private String medicationName;
 }
