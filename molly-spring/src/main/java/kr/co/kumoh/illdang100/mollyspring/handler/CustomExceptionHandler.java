79d2f000e81c5d63eb519da2e1163ebd1d5651da
JYeonJun <97449471+JYeonJun@users.noreply.github.com>
2023-03-17 09:51:12 +0900

    feat(server): validation 공통 예외처리

--- molly-spring/src/main/java/kr/co/kumoh/illdang100/mollyspring/handler/CustomExceptionHandler.java
+++ molly-spring/src/main/java/kr/co/kumoh/illdang100/mollyspring/handler/CustomExceptionHandler.java
@@ -1,17 +1,21 @@
 package kr.co.kumoh.illdang100.mollyspring.handler;
 
+import kr.co.kumoh.illdang100.mollyspring.dto.ResponseDto;
+import kr.co.kumoh.illdang100.mollyspring.handler.ex.CustomValidationException;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @Slf4j
 @RestControllerAdvice
 public class CustomExceptionHandler {
 
-    // 예시
-    /*@ExceptionHandler(CustomApiException.class)
-    public ResponseEntity<?> apiException(CustomApiException e) {
+    @ExceptionHandler(CustomValidationException.class)
+    public ResponseEntity<?> validationApiException(CustomValidationException e) {
 
         log.error(e.getMessage());
-        return new ResponseEntity<>(new ResponseDto<>(-1, e.getMessage(), null), HttpStatus.BAD_REQUEST);
-    }*/
+        return new ResponseEntity<>(new ResponseDto<>(-1, e.getMessage(), e.getErrorMap()), HttpStatus.BAD_REQUEST);
+    }
 }
