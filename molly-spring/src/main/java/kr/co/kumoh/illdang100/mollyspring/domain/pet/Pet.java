09854db0a8da04300426d3cb0bc151bae8fd60dc
wl2258 <77067383+wl2258@users.noreply.github.com>
2023-03-17 10:57:49 +0900

    create(server): Pet 엔티티 필드 추가

--- molly-spring/src/main/java/kr/co/kumoh/illdang100/mollyspring/domain/pet/Pet.java
+++ molly-spring/src/main/java/kr/co/kumoh/illdang100/mollyspring/domain/pet/Pet.java
@@ -1,18 +1,48 @@
 package kr.co.kumoh.illdang100.mollyspring.domain.pet;
 
+import kr.co.kumoh.illdang100.mollyspring.domain.BaseTimeEntity;
+import kr.co.kumoh.illdang100.mollyspring.domain.user.User;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import java.time.LocalDate;
 
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class Pet {
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "PET_TYPE")
+public abstract class Pet extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "pet_id")
     private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @Column(nullable = false, length = 30)
+    private String petName;
+
+    private String profileImage;
+
+    @Column(nullable = false)
+    private LocalDate birthdate;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private PetGenderEnum gender;
+
+    @Column(nullable = false)
+    private boolean neuteredStatus;
+
+    @Column(nullable = false)
+    private double weight;
+
+    @Column(nullable = false)
+    private PetTypeEnum petType;
 }
