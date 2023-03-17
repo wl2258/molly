ee34a2effcffc074f7abd3f13022378f46b2f0ff
wl2258 <77067383+wl2258@users.noreply.github.com>
2023-03-17 10:57:04 +0900

    create(server): Liky 엔티티 필드 추가

--- molly-spring/src/main/java/kr/co/kumoh/illdang100/mollyspring/domain/liky/Liky.java
+++ molly-spring/src/main/java/kr/co/kumoh/illdang100/mollyspring/domain/liky/Liky.java
@@ -1,5 +1,6 @@
 package kr.co.kumoh.illdang100.mollyspring.domain.liky;
 
+import kr.co.kumoh.illdang100.mollyspring.domain.board.Board;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -15,4 +16,11 @@ public class Liky {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "liky_id")
     private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "board_id")
+    private Board board;
+
+    @Column(nullable = false)
+    private int userId;
 }
