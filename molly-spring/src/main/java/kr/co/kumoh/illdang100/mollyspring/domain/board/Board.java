944c68b07548f5727da98885c04199cbecab9ac7
wl2258 <77067383+wl2258@users.noreply.github.com>
2023-03-17 10:55:59 +0900

    create(server): Board 엔티티 필드 추가

--- molly-spring/src/main/java/kr/co/kumoh/illdang100/mollyspring/domain/board/Board.java
+++ molly-spring/src/main/java/kr/co/kumoh/illdang100/mollyspring/domain/board/Board.java
@@ -1,5 +1,8 @@
 package kr.co.kumoh.illdang100.mollyspring.domain.board;
 
+import kr.co.kumoh.illdang100.mollyspring.domain.BaseTimeEntity;
+import kr.co.kumoh.illdang100.mollyspring.domain.pet.PetTypeEnum;
+import kr.co.kumoh.illdang100.mollyspring.domain.user.User;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,10 +12,31 @@ import javax.persistence.*;
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class Board {
+public class Board extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "board_id")
     private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @Column(nullable = false, length = 60)
+    private String title;
+
+    @Column(nullable = false, length = 1000)
+    private String content;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private BoardEnum category;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private PetTypeEnum petType;
+
+    @Column(nullable = false)
+    private int views;
 }
