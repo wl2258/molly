ceef3b5c3f0c1226fe51390d9b7a7bb223e290bb
wl2258 <77067383+wl2258@users.noreply.github.com>
2023-03-17 10:56:53 +0900

    create(server): Comment 엔티티 필드 추가

--- molly-spring/src/main/java/kr/co/kumoh/illdang100/mollyspring/domain/comment/Comment.java
+++ molly-spring/src/main/java/kr/co/kumoh/illdang100/mollyspring/domain/comment/Comment.java
@@ -1,5 +1,7 @@
 package kr.co.kumoh.illdang100.mollyspring.domain.comment;
 
+import kr.co.kumoh.illdang100.mollyspring.domain.BaseTimeEntity;
+import kr.co.kumoh.illdang100.mollyspring.domain.board.Board;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,10 +11,19 @@ import javax.persistence.*;
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class Comment {
+public class Comment extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "comment_id")
     private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "board_id")
+    private Board board;
+
+    @Column(nullable = false)
+    private int userId;
+
+    private String profileImage;
 }
