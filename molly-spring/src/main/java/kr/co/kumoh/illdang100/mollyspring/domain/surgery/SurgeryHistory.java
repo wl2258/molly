4e62a4f0ceb96110a461da8ab30fd26a1166df6c
wl2258 <77067383+wl2258@users.noreply.github.com>
2023-03-17 10:58:08 +0900

    create(server): SurgeryHistory 엔티티 필드 추가

--- molly-spring/src/main/java/kr/co/kumoh/illdang100/mollyspring/domain/surgery/SurgeryHistory.java
+++ molly-spring/src/main/java/kr/co/kumoh/illdang100/mollyspring/domain/surgery/SurgeryHistory.java
@@ -1,10 +1,12 @@
 package kr.co.kumoh.illdang100.mollyspring.domain.surgery;
 
+import kr.co.kumoh.illdang100.mollyspring.domain.pet.Pet;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import java.time.LocalDate;
 
 @Entity
 @Getter
@@ -15,4 +17,12 @@ public class SurgeryHistory {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "surgery_history_id")
     private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "pet_id")
+    private Pet pet;
+
+    private String surgeryName;
+
+    private LocalDate surgeryDate;
 }
