f98483e2d84cc51b56b237e77aa586f83a439393
wl2258 <77067383+wl2258@users.noreply.github.com>
2023-03-22 15:35:12 +0900

    feat(server): Board 빌더 생성

--- molly-spring/src/main/java/kr/co/kumoh/illdang100/mollyspring/domain/board/Board.java
+++ molly-spring/src/main/java/kr/co/kumoh/illdang100/mollyspring/domain/board/Board.java
@@ -2,8 +2,9 @@ package kr.co.kumoh.illdang100.mollyspring.domain.board;
 
 import kr.co.kumoh.illdang100.mollyspring.domain.BaseTimeEntity;
 import kr.co.kumoh.illdang100.mollyspring.domain.pet.PetTypeEnum;
-import kr.co.kumoh.illdang100.mollyspring.domain.user.User;
+import kr.co.kumoh.illdang100.mollyspring.domain.account.Account;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -20,8 +21,8 @@ public class Board extends BaseTimeEntity {
     private Long id;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "user_id")
-    private User user;
+    @JoinColumn(name = "account_id")
+    private Account account;
 
     @Column(nullable = false, length = 60)
     private String title;
@@ -30,13 +31,24 @@ public class Board extends BaseTimeEntity {
     private String content;
 
     @Enumerated(EnumType.STRING)
-    @Column(nullable = false)
+    @Column(nullable = false, length = 10)
     private BoardEnum category;
 
     @Enumerated(EnumType.STRING)
-    @Column(nullable = false)
+    @Column(nullable = false, length = 10)
     private PetTypeEnum petType;
 
     @Column(nullable = false)
     private int views;
+
+    @Builder
+    public Board(Long id, Account account, String title, String content, BoardEnum category, PetTypeEnum petType, int views) {
+        this.id = id;
+        this.account = account;
+        this.title = title;
+        this.content = content;
+        this.category = category;
+        this.petType = petType;
+        this.views = views;
+    }
 }
