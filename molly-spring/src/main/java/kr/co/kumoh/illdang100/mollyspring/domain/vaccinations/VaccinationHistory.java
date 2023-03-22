34efbeca54d13dec0149ad05f0a9be041e6e4a08
wl2258 <77067383+wl2258@users.noreply.github.com>
2023-03-22 15:42:01 +0900

    feat(server): VaccinationHistory 빌더 추가

--- molly-spring/src/main/java/kr/co/kumoh/illdang100/mollyspring/domain/vaccinations/VaccinationHistory.java
+++ molly-spring/src/main/java/kr/co/kumoh/illdang100/mollyspring/domain/vaccinations/VaccinationHistory.java
@@ -3,6 +3,7 @@ package kr.co.kumoh.illdang100.mollyspring.domain.vaccinations;
 import kr.co.kumoh.illdang100.mollyspring.domain.BaseTimeEntity;
 import kr.co.kumoh.illdang100.mollyspring.domain.pet.Pet;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -23,7 +24,17 @@ public class VaccinationHistory extends BaseTimeEntity {
     @JoinColumn(name = "pet_id")
     private Pet pet;
 
+    @Column(nullable = false)
     private LocalDate vaccinationDate;
 
+    @Column(nullable = false, length = 30)
     private String vaccinationName;
+
+    @Builder
+    public VaccinationHistory(Long id, Pet pet, LocalDate vaccinationDate, String vaccinationName) {
+        this.id = id;
+        this.pet = pet;
+        this.vaccinationDate = vaccinationDate;
+        this.vaccinationName = vaccinationName;
+    }
 }
