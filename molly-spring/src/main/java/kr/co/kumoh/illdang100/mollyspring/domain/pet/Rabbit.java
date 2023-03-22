2618640b4df196d2471f4cbf934ad25e8ce1403d
wl2258 <77067383+wl2258@users.noreply.github.com>
2023-03-22 15:40:58 +0900

    feat(server): Rabbit 빌더 및 생성자 어노테이션 추가

--- molly-spring/src/main/java/kr/co/kumoh/illdang100/mollyspring/domain/pet/Rabbit.java
+++ molly-spring/src/main/java/kr/co/kumoh/illdang100/mollyspring/domain/pet/Rabbit.java
@@ -1,14 +1,26 @@
 package kr.co.kumoh.illdang100.mollyspring.domain.pet;
 
-import javax.persistence.DiscriminatorValue;
-import javax.persistence.Entity;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
+import kr.co.kumoh.illdang100.mollyspring.domain.account.Account;
+import kr.co.kumoh.illdang100.mollyspring.domain.image.ImageFile;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDate;
 
 @Entity
 @DiscriminatorValue("Rabbit")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Rabbit extends Pet{
 
     @Enumerated(EnumType.STRING)
-    private RabbitEnum species;
+    @Column(nullable = false)
+    private RabbitEnum rabbitSpecies;
+
+    @Builder
+    public Rabbit(Long id, Account account, String petName, LocalDate birthdate, PetGenderEnum gender, boolean neuteredStatus, double weight, PetTypeEnum petType, ImageFile petProfile, RabbitEnum species) {
+        super(id, account, petName, birthdate, gender, neuteredStatus, weight, petType, petProfile);
+        this.rabbitSpecies = species;
+    }
 }
