f86417ef5f6445bad875d1c969ef87b56bf3ef94
wl2258 <77067383+wl2258@users.noreply.github.com>
2023-03-22 15:38:39 +0900

    feat(server): Cat 빌더 및 생성자 어노테이션 추가

--- molly-spring/src/main/java/kr/co/kumoh/illdang100/mollyspring/domain/pet/Cat.java
+++ molly-spring/src/main/java/kr/co/kumoh/illdang100/mollyspring/domain/pet/Cat.java
@@ -1,14 +1,26 @@
 package kr.co.kumoh.illdang100.mollyspring.domain.pet;
 
-import javax.persistence.DiscriminatorValue;
-import javax.persistence.Entity;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
+import kr.co.kumoh.illdang100.mollyspring.domain.account.Account;
+import kr.co.kumoh.illdang100.mollyspring.domain.image.ImageFile;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDate;
 
 @Entity
 @DiscriminatorValue("Cat")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Cat extends Pet{
 
     @Enumerated(EnumType.STRING)
-       private CatEnum species;
+    @Column(nullable = false)
+    private CatEnum catSpecies;
+
+    @Builder
+    public Cat(Long id, Account account, String petName, LocalDate birthdate, PetGenderEnum gender, boolean neuteredStatus, double weight, PetTypeEnum petType, ImageFile petProfile, CatEnum species) {
+        super(id, account, petName, birthdate, gender, neuteredStatus, weight, petType, petProfile);
+        this.catSpecies = species;
+    }
 }
