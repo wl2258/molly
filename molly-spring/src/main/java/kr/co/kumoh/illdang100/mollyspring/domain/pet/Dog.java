8e71fd42737f06e710effe9aea7456d6f50c66f6
wl2258 <77067383+wl2258@users.noreply.github.com>
2023-03-22 15:38:59 +0900

    feat(server): Dog 빌더 및 생성자 어노테이션 추가

--- molly-spring/src/main/java/kr/co/kumoh/illdang100/mollyspring/domain/pet/Dog.java
+++ molly-spring/src/main/java/kr/co/kumoh/illdang100/mollyspring/domain/pet/Dog.java
@@ -1,14 +1,26 @@
 package kr.co.kumoh.illdang100.mollyspring.domain.pet;
 
-import javax.persistence.DiscriminatorValue;
-import javax.persistence.Entity;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
+import kr.co.kumoh.illdang100.mollyspring.domain.account.Account;
+import kr.co.kumoh.illdang100.mollyspring.domain.image.ImageFile;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDate;
 
 @Entity
 @DiscriminatorValue("Dog")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Dog extends Pet{
 
     @Enumerated(EnumType.STRING)
-    private DogEnum species;
+    @Column(nullable = false)
+    private DogEnum dogSpecies;
+
+    @Builder
+    public Dog(Long id, Account account, String petName, LocalDate birthdate, PetGenderEnum gender, boolean neuteredStatus, double weight, PetTypeEnum petType, ImageFile petProfile, DogEnum species) {
+        super(id, account, petName, birthdate, gender, neuteredStatus, weight, petType, petProfile);
+        this.dogSpecies = species;
+    }
 }
