59325341aeeb233766f721e8e3a0d8a5efaa74cc
JYeonJun <97449471+JYeonJun@users.noreply.github.com>
2023-03-23 13:31:48 +0900

    feat(server): SecurityConfig 설정

--- molly-spring/src/main/java/kr/co/kumoh/illdang100/mollyspring/config/SecurityConfig.java
+++ molly-spring/src/main/java/kr/co/kumoh/illdang100/mollyspring/config/SecurityConfig.java
@@ -1,4 +1,87 @@
 package kr.co.kumoh.illdang100.mollyspring.config;
 
+import kr.co.kumoh.illdang100.mollyspring.config.jwt.JwtAuthorizationFilter;
+import kr.co.kumoh.illdang100.mollyspring.config.oauth.CustomOAuth2UserService;
+import kr.co.kumoh.illdang100.mollyspring.config.oauth.OAuth2SuccessHandler;
+import kr.co.kumoh.illdang100.mollyspring.domain.user.UserEnum;
+import kr.co.kumoh.illdang100.mollyspring.util.CustomResponseUtil;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Slf4j
+@Configuration
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final CustomOAuth2UserService oAuth2UserService;
+    private final OAuth2SuccessHandler oAuth2SuccessHandler;
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    // 모든 필터 등록은 여기서!!
+    public class CustomSecurityFilterManager extends AbstractHttpConfigurer<CustomSecurityFilterManager, HttpSecurity> {
+        @Override
+        public void configure(HttpSecurity http) throws Exception {
+            AuthenticationManager authenticationManager = http.getSharedObject(AuthenticationManager.class);
+//            http.addFilter(new JwtAuthenticationFilter(authenticationManager));
+            http.addFilter(new JwtAuthorizationFilter(authenticationManager));
+        }
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http.headers().frameOptions().disable(); // iframe 허용안함
+        http.csrf().disable(); // csrf 허용안함
+
+        // 인증 실패 처리
+        http.exceptionHandling().authenticationEntryPoint((request, response, authException) -> {
+            CustomResponseUtil.fail(response, "로그인을 진행해 주세요", HttpStatus.UNAUTHORIZED);
+        });
+
+        // 권한 실패
+        http.exceptionHandling().accessDeniedHandler((request, response, e) -> {
+            CustomResponseUtil.fail(response, "권한이 없습니다", HttpStatus.FORBIDDEN);
+        });
+
+        /*
+         * SessionCreationPolicy.STATELESS
+         * 클라이언트가 로그인 request
+         * 서버는 User 세션 저장
+         * 서버가 response
+         * 세션값 사라짐. (즉 유지하지 않음)
+         */
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        http.formLogin().disable();
+        // httpBasic()은 브라우저가 팝업창을 이용해서 사용자 인증을 진행한다.
+        http.httpBasic().disable();
+
+        // 필터 적용
+        http.apply(new CustomSecurityFilterManager());
+
+        http.authorizeHttpRequests()
+                .antMatchers("/api/auth/**").authenticated()
+                .antMatchers("/api/admin/**").hasRole("" + UserEnum.ADMIN)
+                .anyRequest().permitAll();
+
+        http
+                .oauth2Login().loginPage("/token/expired")
+                .defaultSuccessUrl("/login-success")    // oauth2 인증이 성공했을 때, 이동되는 url 설정
+                .successHandler(oAuth2SuccessHandler)
+                .userInfoEndpoint().userService(oAuth2UserService); // 로그인이 성공하면 해당 유저의  정보를 들고 customOAuth2UserService에서 후처리 해주겠다는 의미
+
+        return http.build();
+    }
 }
