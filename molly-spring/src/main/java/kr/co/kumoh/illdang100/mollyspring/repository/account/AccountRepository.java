149780188e352d307ec3f84059137f040ace2b94
JYeonJun <97449471+JYeonJun@users.noreply.github.com>
2023-03-25 17:40:29 +0900

    feat(server): AccountRepository

--- molly-spring/src/main/java/kr/co/kumoh/illdang100/mollyspring/repository/account/AccountRepository.java
+++ molly-spring/src/main/java/kr/co/kumoh/illdang100/mollyspring/repository/account/AccountRepository.java
@@ -3,5 +3,9 @@ package kr.co.kumoh.illdang100.mollyspring.repository.account;
 import kr.co.kumoh.illdang100.mollyspring.domain.account.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface AccountRepository extends JpaRepository<Account, Long> {
+
+    Optional<Account> findByUsername(String username);
 }
