15151cd93812e13c10b3bb11484c5ea33a756c64
JYeonJun <97449471+JYeonJun@users.noreply.github.com>
2023-03-30 00:23:44 +0900

    feat(server): OAuth2SuccessHandler
    리다이렉트 기능 추가

--- molly-spring/src/main/java/kr/co/kumoh/illdang100/mollyspring/config/oauth/OAuth2SuccessHandler.java
+++ molly-spring/src/main/java/kr/co/kumoh/illdang100/mollyspring/config/oauth/OAuth2SuccessHandler.java
@@ -5,12 +5,14 @@ import kr.co.kumoh.illdang100.mollyspring.config.jwt.JwtProcess;
 import kr.co.kumoh.illdang100.mollyspring.config.jwt.JwtVO;
 import kr.co.kumoh.illdang100.mollyspring.domain.account.Account;
 import kr.co.kumoh.illdang100.mollyspring.dto.account.AccountRespDto;
+import kr.co.kumoh.illdang100.mollyspring.repository.account.AccountRepository;
 import kr.co.kumoh.illdang100.mollyspring.util.CustomResponseUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -32,14 +34,23 @@ public class OAuth2SuccessHandler extends SimpleUrlAuthenticationSuccessHandler 
         PrincipalDetails principal = (PrincipalDetails) authentication.getPrincipal();
 
         Account account = principal.getAccount();
-
-        PrincipalDetails loginAccount = (PrincipalDetails) authentication.getPrincipal();
-        String jwtToken = jwtProcess.create(loginAccount);
-        log.info("jwtToken={}", jwtToken);
-
+        String jwtToken = jwtProcess.create(principal);
         response.addHeader(JwtVO.HEADER, jwtToken);
 
-        AccountRespDto.LoginRespDto loginRespDto = new AccountRespDto.LoginRespDto(account.getId(), account.getUsername());
-        CustomResponseUtil.success(response, loginRespDto);
+        // TODO: 인증에 성공하면 접근 실패한 uri를 http://localhost:3000/ 뒤에 추가해서 리다이렉트 시키기!!
+        String additionalInputUri = "";
+
+        if (account.getNickname() == null) {
+            additionalInputUri = "home/signup";
+        }
+
+        String redirectUrl = makeRedirectUrl(additionalInputUri);
+        log.info("redirectUrl={}", redirectUrl);
+
+        getRedirectStrategy().sendRedirect(request, response, redirectUrl);
+    }
+
+    private String makeRedirectUrl(String uri) {
+        return UriComponentsBuilder.fromUriString("http://localhost:3000/" + uri).build().toUriString();
     }
 }
