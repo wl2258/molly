07e11145af09aebc01868b02ccdc2331c95df2cc
JYeonJun <97449471+JYeonJun@users.noreply.github.com>
2023-04-01 00:28:49 +0900

    feat(server): AccountReqDto

--- molly-spring/src/main/java/kr/co/kumoh/illdang100/mollyspring/dto/account/AccountReqDto.java
+++ molly-spring/src/main/java/kr/co/kumoh/illdang100/mollyspring/dto/account/AccountReqDto.java
@@ -1,4 +1,29 @@
 package kr.co.kumoh.illdang100.mollyspring.dto.account;
 
+import lombok.Data;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
+
 public class AccountReqDto {
+
+    @Data
+    public static class InputNicknameRequest {
+
+        @NotEmpty
+        @Pattern(regexp = "^[ㄱ-ㅎ가-힣a-zA-Z]{1,10}$", message = "한글/영문 1~10자 이내로 작성해주세요")
+        private String nickname;
+    }
+
+    @Data
+    public static class SaveAccountRequest {
+
+        @NotEmpty
+        @Pattern(regexp = "^[ㄱ-ㅎ가-힣a-zA-Z]{1,10}$", message = "한글/영문 1~10자 이내로 작성해주세요")
+        private String nickname;
+
+        private MultipartFile accountProfileImage;
+    }
+
 }
