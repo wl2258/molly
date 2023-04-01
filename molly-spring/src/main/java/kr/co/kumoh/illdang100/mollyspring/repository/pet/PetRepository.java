ed2a3b6e5792fa1d88c00700e38f185e50ac1a3b
wl2258 <wl2258@naver.com>
2023-04-01 12:45:50 +0900

    feat(server): PetRepository 생성

---
+++ molly-spring/src/main/java/kr/co/kumoh/illdang100/mollyspring/repository/pet/PetRepository.java
@@ -0,0 +1,12 @@
+package kr.co.kumoh.illdang100.mollyspring.repository.pet;
+
+import kr.co.kumoh.illdang100.mollyspring.domain.pet.Pet;
+import kr.co.kumoh.illdang100.mollyspring.domain.pet.PetTypeEnum;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.LocalDate;
+import java.util.Optional;
+
+public interface PetRepository extends JpaRepository<Pet, Long> {
+    Optional<Pet> findByAccountIdAndPetTypeAndPetNameAndBirthdate(Long accountId, PetTypeEnum petType, String petName, LocalDate birthdate);
+}
