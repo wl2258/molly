d0e37aff75728cf48272f4b542a64417b301fd1c
wl2258 <wl2258@naver.com>
2023-04-05 11:01:23 +0900

    feat(server): SurgeryRespDto dto 추가
    - SurgerySaveResponse
    - SurgeryListResponse
    - SurgeryResponse

---
+++ molly-spring/src/main/java/kr/co/kumoh/illdang100/mollyspring/dto/surgery/SurgeryRespDto.java
@@ -0,0 +1,30 @@
+package kr.co.kumoh.illdang100.mollyspring.dto.surgery;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public class SurgeryRespDto {
+    @Data
+    @AllArgsConstructor
+    public static class SurgerySaveResponse {
+        private Long SurgeryId;
+    }
+
+    @Data
+    @AllArgsConstructor
+    public static class SurgeryListResponse {
+        private List<SurgeryResponse> surgery;
+    }
+
+    @Data
+    @AllArgsConstructor
+    public static class SurgeryResponse {
+        private String surgeryName;
+        @JsonFormat(pattern="yyyy-MM-dd")
+        private LocalDate surgeryDate;
+    }
+}
