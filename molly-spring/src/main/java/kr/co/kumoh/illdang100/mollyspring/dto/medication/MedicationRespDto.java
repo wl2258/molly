db32ce3181be84dfc870a8aacada5c38e5a56efd
wl2258 <wl2258@naver.com>
2023-04-05 10:59:58 +0900

    feat(server): MedicationRespDto dto 추가
    - MedicationSaveResponse
    - MedicationListResponse
    - MedicationResponse

---
+++ molly-spring/src/main/java/kr/co/kumoh/illdang100/mollyspring/dto/medication/MedicationRespDto.java
@@ -0,0 +1,33 @@
+package kr.co.kumoh.illdang100.mollyspring.dto.medication;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public class MedicationRespDto {
+
+    @Data
+    @AllArgsConstructor
+    public static class MedicationSaveResponse {
+        private Long medicationId;
+    }
+
+    @Data
+    @AllArgsConstructor
+    public static class MedicationListResponse {
+        private List<MedicationResponse> medication;
+    }
+
+    @Data
+    @AllArgsConstructor
+    public static class MedicationResponse {
+        private String medicationName;
+        @JsonFormat(pattern="yyyy-MM-dd")
+        private LocalDate medicationStartDate;
+        @JsonFormat(pattern="yyyy-MM-dd")
+        private LocalDate medicationEndDate;
+    }
+}
