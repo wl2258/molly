5c255046183cca57063025b79e04e57bdee41b89
JYeonJun <97449471+JYeonJun@users.noreply.github.com>
2023-04-05 20:00:28 +0900

    create(server): DummyObject

---
+++ molly-spring/src/main/java/kr/co/kumoh/illdang100/mollyspring/config/dummy/DummyObject.java
@@ -0,0 +1,37 @@
+package kr.co.kumoh.illdang100.mollyspring.config.dummy;
+
+import kr.co.kumoh.illdang100.mollyspring.domain.account.Account;
+import kr.co.kumoh.illdang100.mollyspring.domain.account.AccountEnum;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+public class DummyObject {
+
+    protected Account newAccount(String username, String nickname) {
+
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        String encPassword = passwordEncoder.encode("1234");
+
+        return Account.builder()
+                .username(username)
+                .password(encPassword)
+                .email(username + "@naver.com")
+                .nickname(nickname)
+                .role(AccountEnum.CUSTOMER)
+                .build();
+    }
+
+    protected Account newMockUser(Long id, String username, String nickname) {
+
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        String encPassword = passwordEncoder.encode("1234");
+
+        return Account.builder()
+                .id(id)
+                .username(username)
+                .password(encPassword)
+                .email(username + "@naver.com")
+                .nickname(nickname)
+                .role(AccountEnum.CUSTOMER)
+                .build();
+    }
+}
