7d1d34d80bae65c986112263d09ca3241de24e00
wl2258 <wl2258@naver.com>
2023-04-06 23:49:54 +0900

    update(server): Rabbit Column 어노테이션 추가
    - nullable = false

--- molly-spring/src/main/java/kr/co/kumoh/illdang100/mollyspring/domain/pet/Rabbit.java
+++ molly-spring/src/main/java/kr/co/kumoh/illdang100/mollyspring/domain/pet/Rabbit.java
@@ -17,6 +17,7 @@ import java.time.LocalDate;
 public class Rabbit extends Pet{
 
     @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
     private RabbitEnum rabbitSpecies;
 
     @Builder
