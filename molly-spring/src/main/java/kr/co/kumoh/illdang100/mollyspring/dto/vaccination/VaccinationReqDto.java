c3d79f194b8ae24d3af275d9abe49b58d7def456
wl2258 <wl2258@naver.com>
2023-04-07 18:49:18 +0900

    update(server): VaccinationReqDto VaccinationRequest 클래스 삭제

--- molly-spring/src/main/java/kr/co/kumoh/illdang100/mollyspring/dto/vaccination/VaccinationReqDto.java
+++ molly-spring/src/main/java/kr/co/kumoh/illdang100/mollyspring/dto/vaccination/VaccinationReqDto.java
@@ -54,20 +54,4 @@ public class VaccinationReqDto {
         @NotNull
         private Long vaccinationId;
     }
-
-    @Data
-    @Builder
-    @AllArgsConstructor
-    public static class VaccinationRequest {
-
-        @NotNull
-        private Long petId;
-
-        @NotBlank(message = "예방접종 이름은 빈 문자열이면 안됩니다.")
-        private String vaccinationName;
-
-        @DateTimeFormat(pattern="yyyy-MM-dd")
-        @Pattern(regexp = "^\\d{4}-\\d{2}-\\d{2}$", message = "예방접종 날짜는 yyyy-MM-dd 형식이어야 합니다.")
-        private LocalDate vaccinationDate;
-    }
 }
