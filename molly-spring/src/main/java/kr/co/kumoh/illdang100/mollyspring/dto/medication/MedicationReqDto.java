39ded1b9492c12d33478b8948f3ca7f8e41f974e
wl2258 <wl2258@naver.com>
2023-04-07 10:56:42 +0900

    update(server): MedicationReqDto dto 추가
    - MedicationSaveRequest
    - MedicationUpdateRequest
    - MedicationDeleteRequest
    - MedicationRequest

--- molly-spring/src/main/java/kr/co/kumoh/illdang100/mollyspring/dto/medication/MedicationReqDto.java
+++ molly-spring/src/main/java/kr/co/kumoh/illdang100/mollyspring/dto/medication/MedicationReqDto.java
@@ -2,8 +2,10 @@ package kr.co.kumoh.illdang100.mollyspring.dto.medication;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import org.springframework.format.annotation.DateTimeFormat;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 import java.time.LocalDate;
 
 public class MedicationReqDto {
@@ -11,40 +13,65 @@ public class MedicationReqDto {
     @Data
     @AllArgsConstructor
     public static class MedicationSaveRequest {
+
+        @NotNull
         private Long petId;
+
+        @NotBlank(message = "복용약 이름은 빈 문자열이면 안됩니다.")
         private String medicationName;
-        @DateTimeFormat(pattern="yyyy-MM-dd")
+
+        @NotNull
+        @Pattern(regexp = "^\\d{4}-\\d{2}-\\d{2}$", message = "복용약 시작 날짜는 yyyy-MM-dd 형식이어야 합니다.")
         private LocalDate medicationStartDate;
-        @DateTimeFormat(pattern="yyyy-MM-dd")
+
+        @NotNull
+        @Pattern(regexp = "^\\d{4}-\\d{2}-\\d{2}$", message = "복용약 종료 날짜는 yyyy-MM-dd 형식이어야 합니다.")
         private LocalDate medicationEndDate;
     }
 
     @Data
     @AllArgsConstructor
     public static class MedicationUpdateRequest {
+        @NotNull
         private Long petId;
+
+        @NotNull
         private Long medicationId;
+
+        @NotBlank(message = "복용약 이름은 빈 문자열이면 안 됩니다.")
         private String medicationName;
-        @DateTimeFormat(pattern="yyyy-MM-dd")
+
+        @NotNull
+        @Pattern(regexp = "^\\d{4}-\\d{2}-\\d{2}$", message = "복용약 시작 날짜는 yyyy-MM-dd 형식이어야 합니다.")
         private LocalDate medicationStartDate;
-        @DateTimeFormat(pattern="yyyy-MM-dd")
+
+        @NotNull
+        @Pattern(regexp = "^\\d{4}-\\d{2}-\\d{2}$", message = "복용약 종료 날짜는 yyyy-MM-dd 형식이어야 합니다.")
         private LocalDate medicationEndDate;
     }
 
     @Data
     @AllArgsConstructor
     public static class MedicationDeleteRequest {
+        @NotNull
         private Long petId;
+
+        @NotNull
         private Long medicationId;
     }
 
     @Data
     @AllArgsConstructor
     public static class MedicationRequest {
+        @NotBlank(message = "복용약 이름은 빈 문자열이면 안됩니다.")
         private String medicationName;
-        @DateTimeFormat(pattern="yyyy-MM-dd")
+
+        @NotNull
+        @Pattern(regexp = "^\\d{4}-\\d{2}-\\d{2}$", message = "복용약 시작 날짜는 yyyy-MM-dd 형식이어야 합니다.")
         private LocalDate medicationStartDate;
-        @DateTimeFormat(pattern="yyyy-MM-dd")
+
+        @NotNull
+        @Pattern(regexp = "^\\d{4}-\\d{2}-\\d{2}$", message = "복용약 종료 날짜는 yyyy-MM-dd 형식이어야 합니다.")
         private LocalDate medicationEndDate;
     }
 }
