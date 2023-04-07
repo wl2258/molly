d331e0c5604ac5adafd6ef7b4d164cc19aa62be4
wl2258 <wl2258@naver.com>
2023-04-07 11:01:49 +0900

    create(server); VaccinationRespDto 생성
    - VaccinationSaveResponse
    - VaccinationListResponse
    - VaccinationResponse

---
+++ molly-spring/src/main/java/kr/co/kumoh/illdang100/mollyspring/dto/vaccination/VaccinationRespDto.java
@@ -0,0 +1,32 @@
+package kr.co.kumoh.illdang100.mollyspring.dto.vaccination;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.time.LocalDate;
+import java.util.List;
+
+
+public class VaccinationRespDto {
+
+    @Data
+    @AllArgsConstructor
+    public static class VaccinationSaveResponse {
+        private Long vaccinationId;
+    }
+
+    @Data
+    @AllArgsConstructor
+    public static class VaccinationListResponse {
+        private List<VaccinationResponse> vaccination;
+    }
+
+    @Data
+    @AllArgsConstructor
+    public static class VaccinationResponse {
+        private String vaccinationName;
+        @JsonFormat(pattern="yyyy-MM-dd")
+        private LocalDate vaccinationDate;
+    }
+}
