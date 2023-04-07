bb3f8668da0949aff724a7611d3172545a465241
wl2258 <wl2258@naver.com>
2023-04-07 18:48:48 +0900

    update(server): SurgeryReqDto SurgeryRequest 클래스 삭제

--- molly-spring/src/main/java/kr/co/kumoh/illdang100/mollyspring/dto/surgery/SurgeryReqDto.java
+++ molly-spring/src/main/java/kr/co/kumoh/illdang100/mollyspring/dto/surgery/SurgeryReqDto.java
@@ -56,20 +56,4 @@ public class SurgeryReqDto {
         @NotNull
         private Long surgeryId;
     }
-
-    @Data
-    @Builder
-    @AllArgsConstructor
-    public static class SurgeryRequest {
-
-        @NotNull
-        private Long petId;
-
-        @NotBlank(message = "수술명은 빈 문자열이면 안됩니다.")
-        private String surgeryName;
-
-        @DateTimeFormat(pattern="yyyy-MM-dd")
-        @Pattern(regexp = "^\\d{4}-\\d{2}-\\d{2}$", message = "수술 날짜는 yyyy-MM-dd 형식이어야 합니다.")
-        private LocalDate surgeryDate;
-    }
 }
