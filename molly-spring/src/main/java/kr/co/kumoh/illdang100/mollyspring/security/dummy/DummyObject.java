d116d3b7d154502e37eb3561f0eae96060a6a354
wl2258 <wl2258@naver.com>
2023-04-13 11:11:24 +0900

    update(server): DummyObject newPet 메소드 추가

--- molly-spring/src/main/java/kr/co/kumoh/illdang100/mollyspring/security/dummy/DummyObject.java
+++ molly-spring/src/main/java/kr/co/kumoh/illdang100/mollyspring/security/dummy/DummyObject.java
@@ -2,7 +2,6 @@ package kr.co.kumoh.illdang100.mollyspring.security.dummy;
 
 import kr.co.kumoh.illdang100.mollyspring.domain.account.Account;
 import kr.co.kumoh.illdang100.mollyspring.domain.account.AccountEnum;
-import kr.co.kumoh.illdang100.mollyspring.domain.image.ImageFile;
 import kr.co.kumoh.illdang100.mollyspring.domain.pet.*;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
@@ -39,16 +38,14 @@ public class DummyObject {
                 .build();
     }
 
-    protected Pet newMockPet(Long id, Account account, String petName, LocalDate birthdate, PetGenderEnum gender, boolean neuteredStatus, double weight, PetTypeEnum petType, ImageFile petProfileImage, String caution, DogEnum species) {
+    protected Pet newPet(Account account, String petName, LocalDate birthdate, PetGenderEnum gender, boolean neuteredStatus, double weight, PetTypeEnum petType, String caution, DogEnum species) {
         return Dog.builder()
-                .id(id)
                 .account(account)
                 .petName(petName)
                 .gender(gender)
                 .neuteredStatus(neuteredStatus)
                 .weight(weight)
                 .petType(petType)
-                .petProfileImage(petProfileImage)
                 .birthdate(birthdate)
                 .caution(caution)
                 .dogSpecies(species)
