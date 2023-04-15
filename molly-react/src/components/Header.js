726a7e92369b7cd7f4486d8a4c2c115b9f495321
yunzung <yunej27@gmail.com>
2023-04-15 22:35:53 +0900

    design: register pet page update

--- molly-react/src/components/Header.js
+++ molly-react/src/components/Header.js
@@ -198,7 +198,10 @@ const PetDropdown = () => {
     <div className={styles.petdropdown}>
       {pet.map((item, index) => {
         return (
-          item !== '' && <li onClick={() => {navigate(`/detailpet/${index+1}`)}}>{item}</li>
+          item !== '' && 
+            <li onClick={() => {navigate(`/detailpet/${index+1}`)}}>
+              <img className={styles.petimg} src={process.env.PUBLIC_URL + '/img/puppy-logo.png'} alt="puppy" width="36px"/>{item}
+            </li>
         )
       })}
       {pet.length === 0 ? <li style={{borderRadius: "10px"}} onClick={() => {navigate('/registerpet')}}>추가하기</li>:
