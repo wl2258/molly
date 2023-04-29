7c0c68e9610a8c393d7612452e3704d48ce11839
wl2258 <wl2258@naver.com>
2023-04-29 18:12:26 +0900

    update(server): PetApiControllerTest 수정

--- molly-spring/src/test/java/kr/co/kumoh/illdang100/mollyspring/web/PetApiControllerTest.java
+++ molly-spring/src/test/java/kr/co/kumoh/illdang100/mollyspring/web/PetApiControllerTest.java
@@ -59,6 +59,7 @@ class PetApiControllerTest extends DummyObject {
     @Autowired private VaccinationRepository vaccinationRepository;
     @Autowired private MedicationRepository medicationRepository;
 
+    private Pet savedPet;
 
     @BeforeEach
     public void setUp() {
@@ -70,13 +71,8 @@ class PetApiControllerTest extends DummyObject {
     @WithUserDetails(value = "molly", setupBefore = TestExecutionEvent.TEST_EXECUTION)
     void savePet_success() throws Exception {
 
-        // given
-        Account account = newAccount( "user", "testUser");
-        accountRepository.save(account);
-
         // when
         ResultActions resultActions = mockMvc.perform(post("/api/auth/pet")
-                .param("userId", String.valueOf(account.getId()))
                 .param("petType", "CAT")
                 .param("petName", "나비")
                 .param("species", "ABYSSINIAN")
@@ -86,57 +82,26 @@ class PetApiControllerTest extends DummyObject {
                 .param("weight", String.valueOf(5.1))
                 .param("caution", "털이 잘 빠짐")
                 .contentType(MediaType.MULTIPART_FORM_DATA_VALUE));
-
 
         // then
         resultActions.andExpect(status().isCreated());
     }
 
     @Test
-    @DisplayName("등록 - 존재하지 않는 회원일 경우")
-    @WithUserDetails(value = "molly", setupBefore = TestExecutionEvent.TEST_EXECUTION)
-    void savePet_failure_v1() throws Exception {
-
-        // given
-
-        // when
-        ResultActions resultActions = mockMvc.perform(post("/api/auth/pet")
-                .param("userId", String.valueOf(56))
-                .param("petName", "나비")
-                .param("petType", "CAT")
-                .param("species", "ABYSSINIAN")
-                .param("weight", String.valueOf(5.1))
-                .param("caution", "털이 잘 빠짐")
-                .param("birthdate", "2021-01-23")
-                .param("gender", "FEMALE")
-                .param("neuteredStatus", "true")
-                .contentType(MediaType.MULTIPART_FORM_DATA_VALUE));
-
-        // then
-        resultActions.andExpect(status().isBadRequest());
-    }
-
-    @Test
-    @DisplayName("등록 - 이미 등록된 반려동물인 경우")
+    @DisplayName("등록 실패 - 이미 등록된 반려동물인 경우")
     @WithUserDetails(value = "molly", setupBefore = TestExecutionEvent.TEST_EXECUTION)
     void savePet_failure_v2() throws Exception {
 
         //given
-        Account account = newAccount( "molly00", "nickname00");
-        accountRepository.save(account);
-
-        Pet pet = newPet( account, "몽이", LocalDate.now().minusYears(1), PetGenderEnum.MALE, true, 3.5, PetTypeEnum.DOG, null, DogEnum.BICHON_FRIZE);
-        petRepository.save(pet);
 
         //when
         ResultActions resultActions = mockMvc.perform(post("/api/auth/pet")
-                .param("userId", String.valueOf(account.getId()))
-                .param("petName", "몽이")
+                .param("petName", "몰리")
                 .param("petType", "DOG")
-                .param("species", "BICHON_FRIZE")
+                .param("species", DogEnum.CHIHUAHUA.toString())
                 .param("weight", String.valueOf(3.5))
                 .param("birthdate", LocalDate.now().minusYears(1).toString())
-                .param("gender", "MALE")
+                .param("gender", "FEMALE")
                 .param("neuteredStatus", "true")
                 .contentType(MediaType.MULTIPART_FORM_DATA_VALUE));
 
@@ -150,24 +115,17 @@ class PetApiControllerTest extends DummyObject {
     void updatePet_success() throws Exception {
 
         //given
-        Account account = newAccount( "test3", "testUser3");
-        accountRepository.save(account);
-
-        Pet pet = newPet( account, "강아지", LocalDate.now().minusYears(3), PetGenderEnum.MALE, true, 3.7, PetTypeEnum.DOG, null, DogEnum.AUSTRALIAN_SILKY_TERRIER);
-        petRepository.save(pet);
 
         //when
         ResultActions resultActions = mockMvc.perform(patch("/api/auth/pet")
-                .param("userId", String.valueOf(account.getId()))
-                .param("petId", String.valueOf(pet.getId()))
+                .param("petId", String.valueOf(savedPet.getId()))
                 .param("petName", "강아지")
                 .param("petType", "DOG")
                 .param("species", DogEnum.AUSTRALIAN_SILKY_TERRIER.toString())
-                .param("weight", String.valueOf(4.0))
-                .param("birthdate", "2022-04-10")
-                .param("gender", "MALE")
+                .param("weight", String.valueOf(3.0))
+                .param("birthdate", LocalDate.now().minusYears(3).toString())
+                .param("gender", "FEMALE")
                 .param("neuteredStatus", "true")
-                .param("caution", "물릴 수도 있음")
                 .contentType(MediaType.MULTIPART_FORM_DATA_VALUE));
 
         //then
@@ -175,17 +133,14 @@ class PetApiControllerTest extends DummyObject {
     }
 
     @Test
-    @DisplayName("수정 - 존재하지 않는 반려동물인 경우")
+    @DisplayName("수정 실패 - 존재하지 않는 반려동물인 경우")
     @WithUserDetails(value = "molly", setupBefore = TestExecutionEvent.TEST_EXECUTION)
     void updatePet_failure() throws Exception {
 
         //given
-        Account account = newAccount( "test3", "testUser3");
-        accountRepository.save(account);
 
         //when
         ResultActions resultActions = mockMvc.perform(patch("/api/auth/pet")
-                .param("userId", String.valueOf(account.getId()))
                 .param("petId", String.valueOf(56))
                 .param("petName", "몽이")
                 .param("petType", "DOG")
@@ -206,15 +161,10 @@ class PetApiControllerTest extends DummyObject {
     @WithUserDetails(value = "molly", setupBefore = TestExecutionEvent.TEST_EXECUTION)
     void viewDetails_success() throws Exception {
 
-        //given
-        Account account = newAccount( "test4", "testUser4");
-        accountRepository.save(account);
-
-        Pet pet = newPet( account, "강아지", LocalDate.now().minusYears(3), PetGenderEnum.MALE, true, 3.7, PetTypeEnum.DOG, null, DogEnum.AUSTRALIAN_SILKY_TERRIER);
-        petRepository.save(pet);
+        // given
 
         //when
-        ResultActions resultActions = mockMvc.perform(get("/api/auth/pet/" + String.valueOf(pet.getId())).contentType(MediaType.APPLICATION_JSON_VALUE));
+        ResultActions resultActions = mockMvc.perform(get("/api/auth/pet/" + savedPet.getId().toString()).contentType(MediaType.APPLICATION_JSON_VALUE));
 
         //then
         resultActions.andExpect(status().isOk());
@@ -222,7 +172,7 @@ class PetApiControllerTest extends DummyObject {
 
 
     @Test
-    @DisplayName("상세보기 - 존재하지 않는 반려동물인 경우")
+    @DisplayName("상세보기 실패 - 존재하지 않는 반려동물인 경우")
     @WithUserDetails(value = "molly", setupBefore = TestExecutionEvent.TEST_EXECUTION)
     void viewDetails_failure() throws Exception {
 
@@ -240,22 +190,16 @@ class PetApiControllerTest extends DummyObject {
     @WithUserDetails(value = "molly", setupBefore = TestExecutionEvent.TEST_EXECUTION)
     void deletePet_success() throws Exception {
 
-        //given
-        Account account = newAccount( "test6", "testUser!");
-        accountRepository.save(account);
-
-        Pet pet = newPet(account, "멍뭉", LocalDate.now().minusYears(1), PetGenderEnum.MALE, true, 5.1, PetTypeEnum.DOG, null, DogEnum.BOXER);
-        petRepository.save(pet);
-
+        // given
         SurgeryHistory surgery = SurgeryHistory.builder()
-                .pet(pet)
+                .pet(savedPet)
                 .surgeryName("surgery1")
                 .surgeryDate(LocalDate.now().minusDays(3))
                 .build();
         surgeryRepository.save(surgery);
 
         MedicationHistory medication = MedicationHistory.builder()
-                .pet(pet)
+                .pet(savedPet)
                 .medicationName("medication1")
                 .medicationStartDate(LocalDate.now().minusDays(7))
                 .medicationEndDate(LocalDate.now().plusDays(7))
@@ -263,14 +207,14 @@ class PetApiControllerTest extends DummyObject {
         medicationRepository.save(medication);
 
         VaccinationHistory vaccination = VaccinationHistory.builder()
-                .pet(pet)
+                .pet(savedPet)
                 .vaccinationName("vaccination1")
                 .vaccinationDate(LocalDate.of(2022, 4, 2))
                 .build();
         vaccinationRepository.save(vaccination);
 
         //when
-        ResultActions resultActions = mockMvc.perform(delete("/api/auth/pet/" + String.valueOf(pet.getId())).contentType(MediaType.APPLICATION_JSON_VALUE));
+        ResultActions resultActions = mockMvc.perform(delete("/api/auth/pet/" + savedPet.getId().toString()).contentType(MediaType.APPLICATION_JSON_VALUE));
 
         //then
         resultActions.andExpect(status().isOk());
@@ -291,36 +235,11 @@ class PetApiControllerTest extends DummyObject {
     }
 
     private void dataSetting() {
-
-        accountRepository.save(newAccount("molly", "일당백"));
-
-        Account account = newAccount( "user2", "testUser2");
+        Account account = newAccount("molly", "일당백");
         accountRepository.save(account);
 
-        Pet pet = newPet(account, "강쥐", LocalDate.now().minusYears(1), PetGenderEnum.MALE, true, 5.1, PetTypeEnum.DOG, null, DogEnum.BOXER);
-        petRepository.save(pet);
-
-        SurgeryHistory surgery = SurgeryHistory.builder()
-                .pet(pet)
-                .surgeryName("surgery1")
-                .surgeryDate(LocalDate.now().minusDays(3))
-                .build();
-        surgeryRepository.save(surgery);
-
-        MedicationHistory medication = MedicationHistory.builder()
-                .pet(pet)
-                .medicationName("medication1")
-                .medicationStartDate(LocalDate.now().minusDays(7))
-                .medicationEndDate(LocalDate.now().plusDays(7))
-                .build();
-        medicationRepository.save(medication);
-
-        VaccinationHistory vaccination = VaccinationHistory.builder()
-                .pet(pet)
-                .vaccinationName("vaccination1")
-                .vaccinationDate(LocalDate.of(2022, 4, 2))
-                .build();
-        vaccinationRepository.save(vaccination);
+        Pet pet = newPet(account, "몰리", LocalDate.now().minusYears(1), PetGenderEnum.FEMALE, true, 3.5, PetTypeEnum.DOG, null, DogEnum.CHIHUAHUA);
+        savedPet =  petRepository.save(pet);
 
         em.clear();
     }
