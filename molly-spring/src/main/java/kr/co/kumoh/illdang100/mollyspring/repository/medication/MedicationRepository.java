94d07d1983975fd7fb3266b778ae685b189b25bd
wl2258 <wl2258@naver.com>
2023-05-01 22:25:08 +0900

    feat(server): existsMedicationByPet 메서드 생성

--- molly-spring/src/main/java/kr/co/kumoh/illdang100/mollyspring/repository/medication/MedicationRepository.java
+++ molly-spring/src/main/java/kr/co/kumoh/illdang100/mollyspring/repository/medication/MedicationRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface MedicationRepository extends JpaRepository<MedicationHistory, Long> {
+public interface MedicationRepository extends JpaRepository<MedicationHistory, Long>, MedicationRepositoryCustom {
 
     List<MedicationHistory> findByPet_Id(Long petId);
 
