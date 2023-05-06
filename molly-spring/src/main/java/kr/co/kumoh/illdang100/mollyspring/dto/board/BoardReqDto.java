2fc0b541898d689152c80ee32f22602614069bfb
JYeonJun <97449471+JYeonJun@users.noreply.github.com>
2023-05-06 23:55:46 +0900

    [CREATE](server): CreatePostRequest

--- molly-spring/src/main/java/kr/co/kumoh/illdang100/mollyspring/dto/board/BoardReqDto.java
+++ molly-spring/src/main/java/kr/co/kumoh/illdang100/mollyspring/dto/board/BoardReqDto.java
@@ -1,4 +1,24 @@
 package kr.co.kumoh.illdang100.mollyspring.dto.board;
 
+import kr.co.kumoh.illdang100.mollyspring.domain.board.BoardEnum;
+import kr.co.kumoh.illdang100.mollyspring.domain.image.BoardImage;
+import kr.co.kumoh.illdang100.mollyspring.domain.pet.PetTypeEnum;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
 public class BoardReqDto {
+
+    @Getter
+    @Setter
+    public static class CreatePostRequest {
+
+        private String title;
+        private String content;
+        private List<BoardImage> boardImages;
+        private BoardEnum category;
+        private PetTypeEnum petType;
+
+    }
 }
