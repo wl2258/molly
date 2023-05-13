7207e9edff4b0fe55976e44d0ca4d0037b3d4be3
wl2258 <wl2258@naver.com>
2023-05-13 18:49:34 +0900

    update(server): VaccinationRepository findByVaccinationName 메소드 추가

--- molly-spring/src/main/java/kr/co/kumoh/illdang100/mollyspring/repository/vaccination/VaccinationRepository.java
+++ molly-spring/src/main/java/kr/co/kumoh/illdang100/mollyspring/repository/vaccination/VaccinationRepository.java
@@ -4,10 +4,13 @@ import kr.co.kumoh.illdang100.mollyspring.domain.vaccinations.VaccinationHistory
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface VaccinationRepository extends JpaRepository<VaccinationHistory, Long>, VaccinationRepositoryCustom {
 
     List<VaccinationHistory> findByPet_Id(Long petId);
 
     List<VaccinationHistory> findByPet_IdOrderByVaccinationDateAsc(Long petId);
+    
+    Optional<VaccinationHistory> findByVaccinationName(String vaccinationName);
 }
