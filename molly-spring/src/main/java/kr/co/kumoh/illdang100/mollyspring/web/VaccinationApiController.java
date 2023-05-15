7331cf6e0fc387ec902d5405804143bc1051e314
wl2258 <wl2258@naver.com>
2023-05-15 18:49:49 +0900

    update(server): VaccinationApiController

--- molly-spring/src/main/java/kr/co/kumoh/illdang100/mollyspring/web/VaccinationApiController.java
+++ molly-spring/src/main/java/kr/co/kumoh/illdang100/mollyspring/web/VaccinationApiController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
+
 import java.util.List;
 
 import static kr.co.kumoh.illdang100.mollyspring.dto.vaccination.VaccinationReqDto.*;
@@ -26,33 +27,34 @@ public class VaccinationApiController {
     @PostMapping
     public ResponseEntity<?> saveVaccination(@RequestBody @Valid VaccinationSaveRequest request) {
 
-        Long VaccinationId = vaccinationService.saveVaccination(request);
+        VaccinationSaveResponse vaccinationSaveResponse = vaccinationService.saveVaccination(request);
 
-        return new ResponseEntity<>(new ResponseDto<>(1, "예방접종 기록 저장 성공", new VaccinationSaveResponse(VaccinationId)), HttpStatus.CREATED);
+        return new ResponseEntity<>(new ResponseDto<>(1, "예방접종 기록 저장 성공", vaccinationSaveResponse), HttpStatus.CREATED);
+    }
+
+    @PostMapping("{ptId}")
+    public ResponseEntity<?> updateVaccination(@PathVariable Long petId, @RequestBody @Valid VaccinationUpdateRequest request) {
+
+        VaccinationUpdateResponse vaccinationUpdateResponse = vaccinationService.updateVaccination(petId, request);
+
+        return new ResponseEntity<>(new ResponseDto<>(1, "예방접종 기록 수정 성공", vaccinationUpdateResponse), HttpStatus.OK);
+
     }
 
     @GetMapping("{petId}")
-    public ResponseEntity<?> viewVaccinationList(@PathVariable @NotNull Long petId) {
+    public ResponseEntity<?> viewVaccination(@PathVariable @NotNull Long petId) {
 
-        List<VaccinationResponse> VaccinationList = vaccinationService.viewVaccinationList(petId);
+        List<VaccinationResponse> vaccinationResponses = vaccinationService.viewVaccinationList(petId);
 
-        return new ResponseEntity<>(new ResponseDto<>(1, "예방접종 기록 조회 성공", new VaccinationListResponse(VaccinationList)), HttpStatus.OK);
-    }
-
-    @PutMapping
-    public ResponseEntity<?> updateVaccination(@RequestBody @Valid VaccinationUpdateRequest request) {
-
-        vaccinationService.updateVaccination(request);
-
-        return new ResponseEntity<>(new ResponseDto<>(1, "예방접종 기록 수정 성공", null), HttpStatus.OK);
+        return  new ResponseEntity<>(new ResponseDto<>(1, "예방접종 기록 조회 성공", vaccinationResponses), HttpStatus.OK);
 
     }
 
     @DeleteMapping
     public ResponseEntity<?> deleteVaccination(@RequestBody @Valid VaccinationDeleteRequest request) {
 
-        vaccinationService.deleteVaccination(request);
+        VaccinationUpdateResponse vaccinationDeleteResponse = vaccinationService.deleteVaccination(request);
 
-        return new ResponseEntity<>(new ResponseDto<>(1, "예방접종 기록 삭제 성공", null), HttpStatus.OK);
+        return new ResponseEntity<>(new ResponseDto<>(1, "예방접종 기록 삭제 성공", vaccinationDeleteResponse), HttpStatus.OK);
     }
 }
