d0bf569715d5c5c0364ff704b348e1ff835da912
wl2258 <wl2258@naver.com>
2023-05-15 18:48:38 +0900

    update(server): MedicationApiController

--- molly-spring/src/main/java/kr/co/kumoh/illdang100/mollyspring/web/MedicationApiController.java
+++ molly-spring/src/main/java/kr/co/kumoh/illdang100/mollyspring/web/MedicationApiController.java
@@ -10,39 +10,44 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+
+
 import java.util.List;
 
 import static kr.co.kumoh.illdang100.mollyspring.dto.medication.MedicationReqDto.*;
 import static kr.co.kumoh.illdang100.mollyspring.dto.medication.MedicationRespDto.*;
+
 @Slf4j
 @RestController
 @RequestMapping("/api/auth/pet/medication")
 @RequiredArgsConstructor
 public class MedicationApiController {
     private final MedicationService medicationService;
+
     @PostMapping
     public ResponseEntity<?> saveMedication(@RequestBody @Valid MedicationSaveRequest request, BindingResult bindingResult) {
 
-        Long medicationId = medicationService.saveMedication(request);
+        Long petId = medicationService.saveMedication(request);
 
-        return new ResponseEntity<>(new ResponseDto<>(1, "복용 약 이력 저장 성공", new MedicationSaveResponse(medicationId)), HttpStatus.CREATED);
+        return new ResponseEntity<>(new ResponseDto<>(1, "복용 약 이력 저장 성공", new MedicationSaveResponse(petId)), HttpStatus.OK);
     }
 
-    @GetMapping("{petId}")
-    public ResponseEntity<?> viewMedicationList(@PathVariable @Valid Long petId) {
+    @PostMapping("{petId}")
+    public ResponseEntity<?> updateMedication(@PathVariable Long petId, @RequestBody @Valid MedicationUpdateRequest request,    BindingResult bindingResult) {
 
-        List<MedicationResponse> medicationList = medicationService.viewMedicationList(petId);
-
-        return  new ResponseEntity<>(new ResponseDto<>(1, "복용 약 이력 조회 성공", new MedicationListResponse(medicationList)), HttpStatus.OK);
-    }
-
-    @PutMapping
-    public ResponseEntity<?> updateMedication(@RequestBody @Valid MedicationUpdateRequest request, BindingResult bindingResult) {
-
-        medicationService.updateMedication(request);
+        medicationService.updateMedication(petId, request);
 
         return  new ResponseEntity<>(new ResponseDto<>(1, "복용 약 이력 수정 성공", null), HttpStatus.OK);
 
+    }
+
+    @GetMapping("{petId}")
+    public ResponseEntity<?> viewMedication(@PathVariable @NotNull Long petId) {
+
+        List<MedicationResponse> medicationResponses = medicationService.viewMedicationList(petId);
+
+        return  new ResponseEntity<>(new ResponseDto<>(1, "복용 약 조회 성공", medicationResponses), HttpStatus.OK);
     }
 
     @DeleteMapping
