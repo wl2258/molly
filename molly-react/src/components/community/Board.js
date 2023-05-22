c9bfe4e087ba5cb5a7ae849e78d5a89586224074
yunzung <yunej27@gmail.com>
2023-05-23 01:21:31 +0900

    feat(front) : community list axios

--- molly-react/src/components/community/Board.js
+++ molly-react/src/components/community/Board.js
@@ -1,20 +1,25 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from '../../css/Board.module.css';
 import { Button } from '../Button';
 import { MdSearch } from 'react-icons/md';
 import { useNavigate, useParams } from 'react-router-dom';
 
 const Board = () => {
-  const [tap, setTap] = useState('전체');
+  const {pet} = useParams();
+  const [tap, setTap] = useState(pet);
   let navigate = useNavigate();
   let { category } = useParams();
 
+  useEffect(() => {
+    setTap(pet)
+  }, [pet])
+  
   return (
     <div>
-      <Tap setTap={setTap} name={"전체"} tap={tap === '전체'? styles.click : styles.tap} style={styles.all}/>
-      <Tap setTap={setTap} name={"강아지"} tap={tap === '강아지'? styles.click : styles.tap} style={styles.dog}/>
-      <Tap setTap={setTap} name={"고양이"} tap={tap === '고양이'? styles.click : styles.tap} style={styles.cat}/>
-      <Tap setTap={setTap} name={"토끼"} tap={tap === '토끼'? styles.click : styles.tap} style={styles.rabbit}/>
+      <Tap setTap={setTap} name={"NOT_SELECTED"} tap={tap === 'NOT_SELECTED' ? styles.click : styles.tap} style={styles.all}/>
+      <Tap setTap={setTap} name={"DOG"} tap={tap === 'DOG'? styles.click : styles.tap} style={styles.dog}/>
+      <Tap setTap={setTap} name={"CAT"} tap={tap === 'CAT'? styles.click : styles.tap} style={styles.cat}/>
+      <Tap setTap={setTap} name={"RABBIT"} tap={tap === 'RABBIT'? styles.click : styles.tap} style={styles.rabbit}/>
       
       <div className={styles.container}>
         <div>
@@ -30,9 +35,9 @@ const Board = () => {
 const Tap = (props) => {
   let navigate = useNavigate();
   let { category } = useParams();
-  let petType = props.name === "전체" ? "ALL" : 
-    props.name === "강아지" ? "DOG" : 
-    props.name === "고양이" ? "CAT" : "RABBIT"
+  let petType = props.name === "NOT_SELECTED" ? "NOT_SELECTED" : 
+    props.name === "DOG" ? "DOG" : 
+    props.name === "CAT" ? "CAT" : "RABBIT"
 
   return (
     <div 
@@ -41,7 +46,9 @@ const Tap = (props) => {
         props.setTap(props.name)
         navigate(`/list/${category}/${petType}`)
       }}>
-      {props.name}
+      { props.name === "NOT_SELECTED" ? "전체" :
+      props.name === "DOG" ? "강아지" : 
+      props.name === "CAT" ? "고양이" : "토끼"}
     </div>
   )
 }
