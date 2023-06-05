1ffe880b65d4f114d5b3ba349fca24bc2dba45e2
JYeonJun <97449471+JYeonJun@users.noreply.github.com>
2023-06-06 02:02:20 +0900

    [UPDATE](server): 토큰 재발급시 정지된 사용자 제한

--- molly-spring/src/main/java/kr/co/kumoh/illdang100/mollyspring/service/TokenService.java
+++ molly-spring/src/main/java/kr/co/kumoh/illdang100/mollyspring/service/TokenService.java
@@ -1,5 +1,9 @@
 package kr.co.kumoh.illdang100.mollyspring.service;
 
+import kr.co.kumoh.illdang100.mollyspring.domain.account.Account;
+import kr.co.kumoh.illdang100.mollyspring.handler.ex.SuspensionException;
+import kr.co.kumoh.illdang100.mollyspring.repository.account.AccountRepository;
+import kr.co.kumoh.illdang100.mollyspring.repository.suspension.SuspensionDateRepository;
 import kr.co.kumoh.illdang100.mollyspring.security.jwt.JwtProcess;
 import kr.co.kumoh.illdang100.mollyspring.security.jwt.JwtVO;
 import kr.co.kumoh.illdang100.mollyspring.security.jwt.RefreshToken;
@@ -11,6 +15,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.servlet.http.HttpServletResponse;
+import java.time.LocalDate;
 
 @Slf4j
 @Service
@@ -20,44 +25,68 @@ public class TokenService {
 
     private final JwtProcess jwtProcess;
     private final RefreshTokenRedisRepository refreshTokenRedisRepository;
+    private final AccountRepository accountRepository;
+    private final SuspensionDateRepository suspensionDateRepository;
 
     @Transactional
     public void reIssueToken(HttpServletResponse response, String refreshToken) {
+
+        // TODO: 정지 검사하기!!
 
         // 1. Request Header에서 JWT Token 추출
         String extractedToken = extractToken(refreshToken);
 
         // refresh token 유효성 검사
+        RefreshToken findRefreshToken = validateRefreshToken(refreshToken, extractedToken);
+
+        // 저장된 refresh token 정보를 바탕으로 새로운 JWT Token 생성
+        String accountId = findRefreshToken.getId();
+        String accountRole = findRefreshToken.getRole();
+
+        // 만약 정지된 사용자라면 예외 발생
+        validateAccountNotSuspended(accountId);
+
+        String reAccessToken = jwtProcess.createNewAccessToken(Long.valueOf(accountId), accountRole);
+        String reRefreshToken =jwtProcess.createRefreshToken(accountId, accountRole);
+
+        findRefreshToken.changeRefreshToken(reRefreshToken);
+        refreshTokenRedisRepository.save(findRefreshToken);
+
+        response.addHeader("AccountId", accountId);
+        response.addHeader(JwtVO.ACCESS_TOKEN_HEADER, reAccessToken);
+        response.addHeader(JwtVO.REFRESH_TOKEN_HEADER, reRefreshToken);
+    }
+
+    private void validateAccountNotSuspended(String accountId) {
+        Account findAccount = findAccountByIdOrThrowException(Long.valueOf(accountId));
+        suspensionDateRepository.findByAccountEmail(findAccount.getEmail()).ifPresent(suspensionDate -> {
+            log.info("정지된 사용자 계정입니다.");
+            if (!LocalDate.now().isAfter(suspensionDate.getSuspensionExpiryDate())) {
+                throw new SuspensionException("정지된 사용자 계정입니다. 정지 기간:" + suspensionDate.getSuspensionExpiryDate());
+            }
+        });
+    }
+
+    private RefreshToken validateRefreshToken(String refreshToken, String extractedToken) {
         try {
             jwtProcess.isSatisfiedToken(extractedToken);
         } catch (Exception e) {
             throw new CustomApiException("유효하지 않은 토큰입니다");
         }
 
-        // 2. 저장된 refresh token 찾기
-        RefreshToken findRefreshToken = refreshTokenRedisRepository
+        // 저장된 refresh token 찾기
+        return refreshTokenRedisRepository
                 .findByRefreshToken(refreshToken)
                 .orElseThrow(() -> new CustomApiException("토큰 갱신에 실패했습니다"));
-
-        // 3. 저장된 refresh token 정보를 바탕으로 새로운 JWT Token 생성
-        String accountId = findRefreshToken.getId();
-        String accountRole = findRefreshToken.getRole();
-
-        String reAccessToken =
-                jwtProcess.createNewAccessToken(Long.valueOf(accountId), accountRole);
-
-        // 새로운 refresh token을 발급함으로써 refresh token이 탈취되어도 아무런 문제가 없게 된다.
-        String reRefreshToken =
-                jwtProcess.createRefreshToken(accountId, accountRole);
-
-        findRefreshToken.changeRefreshToken(reRefreshToken);
-        refreshTokenRedisRepository.save(findRefreshToken);
-
-        response.addHeader(JwtVO.ACCESS_TOKEN_HEADER, reAccessToken);
-        response.addHeader(JwtVO.REFRESH_TOKEN_HEADER, reRefreshToken);
     }
 
     private static String extractToken(String refreshToken) {
         return refreshToken.replace(JwtVO.TOKEN_PREFIX, "");
+    }
+
+    private Account findAccountByIdOrThrowException(Long accountId) {
+        return accountRepository
+                .findById(accountId)
+                .orElseThrow(() -> new CustomApiException("존재하지 않는 사용자입니다"));
     }
 }
