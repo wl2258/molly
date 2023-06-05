80f7634e98fde48c28fc5e4a2245098b8cd192b0
JYeonJun <97449471+JYeonJun@users.noreply.github.com>
2023-06-05 18:42:31 +0900

    [REMOVE](server): Remove ComplaintRepository

--- molly-spring/src/main/java/kr/co/kumoh/illdang100/mollyspring/repository/complaint/ComplaintRepository.java
+++
@@ -1,7 +0,0 @@
-package kr.co.kumoh.illdang100.mollyspring.repository.complaint;
-
-import kr.co.kumoh.illdang100.mollyspring.domain.complaint.Complaint;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ComplaintRepository extends JpaRepository<Complaint, Long> {
-}
