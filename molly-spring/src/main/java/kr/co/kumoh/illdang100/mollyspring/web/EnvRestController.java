4ca5baec55ea28093c2041fb53c22ca530d67e9b
jimin <wl2258@naver.com>
2023-12-27 15:54:36 +0900

    Update EnvRestController.java

--- molly-spring/src/main/java/kr/co/kumoh/illdang100/mollyspring/web/EnvRestController.java
+++ molly-spring/src/main/java/kr/co/kumoh/illdang100/mollyspring/web/EnvRestController.java
@@ -22,7 +22,7 @@ public class EnvRestController {
         this.mollyConfigUtil = mollyConfigUtil;
     }
 
-    @GetMapping("/molly-profile")
+    @GetMapping("/profile")
     public String getProfile() {
         String currentProfile = Arrays.stream(env.getActiveProfiles())
                 .filter(mollyConfigUtil.getSERVICE_PROFILE_LIST()::contains)
